6667778f98e17a3a986dfb29e27d6b8d99afef85
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-06-09 22:11:13 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/servicereaderjob/ServiceReaderJobApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.servicereaderjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceReaderJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceReaderJobApplication.class, args);
+	}
+
+}
