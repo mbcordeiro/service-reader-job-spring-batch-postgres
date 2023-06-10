90c0ee417daaf4a632f8372aa4242cd187e3e3e1
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-06-09 22:22:07 -0300

    Add step

---
+++ src/main/java/com/matheuscordeiro/servicereaderjob/step/ServiceReaderStepConfig.java
@@ -0,0 +1,27 @@
+package com.matheuscordeiro.servicereaderjob.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.matheuscordeiro.servicereaderjob.domain.User;
+
+@Configuration
+public class ServiceReaderStepConfig {
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Value("${chunkSize}")
+	private int chunkSize;
+
+	@Bean
+	public Step step(ItemReader<User> reader, ItemWriter<User> writer) {
+		return stepBuilderFactory.get("serviceReaderStep").<User, User>chunk(chunkSize).reader(reader).writer(writer)
+				.build();
+	}
+}
